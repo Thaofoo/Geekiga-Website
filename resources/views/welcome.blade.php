4e333a6673f44267665b4aec6b7e4a8058fa5730
Thaofoo <91123146+Thaofoo@users.noreply.github.com>
2023-03-08 07:35:34 +0700

    link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,20 +16,23 @@
                 <div class="navbar_logged_off_left">
                     <img src="{{ asset('images/GEEKIGA LOGO.png') }}" height="30px">
                 </div>
-                <!-- <div class="navbar_logged_off_right">
+                <div class="navbar_logged_off_right">
                     <a href="signUp.html" class="navbar_logged_off_right_signup">
-                        <img src="/public/interface_assets/Person.svg" height="16px">
-                        <img src="/public/interface_assets/Line.svg" height="16px">
+                        <img src="{{ asset('interface_assets/Person.svg') }}" height="16px">
+                        <img src="{{ asset('interface_assets/Line.svg') }}" height="16px">
                         Sign Up
                     </a>
-                    or -->
-                <a href="{{ URL::to('login') }}" class="navbar_logged_off_right_login">
-                    <img src="{{ asset('interface_assets/Person.svg') }}" height="16px">
-                    <img src="{{ asset('interface_assets/Line.svg') }}" height="16px">
-                    Sign In
-                </a>
-        </div>
-        </nav>
+                    or
+                    <!-- <a href="{{ URL::to('login') }}" class="navbar_logged_off_right_login">
+                        <img src="{{ asset('interface_assets/Person.svg') }}" height="16px">
+                        <img src="{{ asset('interface_assets/Line.svg') }}" height="16px">
+                        Sign In
+                    </a> -->
+                    <a href="{{ URL::to('login') }}" class="navbar_logged_off_right_login2">
+                        Sign In
+                    </a>
+                </div>
+            </nav>
         </div>
 
         <div class="hero">
