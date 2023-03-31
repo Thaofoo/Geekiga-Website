40932dfde2f423114eb4d32104d199679a16fa9f
Thaofoo <91123146+Thaofoo@users.noreply.github.com>
2023-03-31 08:10:54 +0700

    homepage and navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,42 +29,62 @@
         </div>
 
         <div class="contents">
+
+            <div class="contents_continue category_container">
+                <div class="category_text">Continue Watching</div>
+                <div class="thumbnails_container">
+                    <div class="thumbnail_segment">
+                        <div class="thumbnail_clip" style="background-image: linear-gradient(to top, #000000, #ffffff00), url('{{ asset('images/thumbnails/demonslayer.webp') }}')">
+                        <div class="progress_bar" id="segment-1"></div>
+                        </div>
+                        Demon Slayer
+                    </div>
+                    <div class="thumbnail_segment">
+                        <div class="thumbnail_clip" style="background-image: linear-gradient(to top, #000000, #ffffff00), url('{{ asset('images/thumbnails/gabi.jpg') }}')">
+                        <div class="progress_bar" id="segment-2"></div>
+                        </div>
+                        Shingeki No Kyojin
+                    </div>
+                </div>
+
+            </div>
+
             <div class="contents_popular category_container">
                 <div class="category_text">Popular Show</div>
                 <div class="posters_container">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
                 </div>
             </div>
 
             <div class="contents_popular category_container">
                 <div class="category_text">Popular Show</div>
                 <div class="posters_container">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
                 </div>
             </div>
 
             <div class="contents_popular category_container">
                 <div class="category_text">Popular Show</div>
                 <div class="posters_container">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
-                    <img class="contents_poster" src="{{ asset('images/posters/AOT.png') }}">
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
+                    <div class="poster_clip" style="background-image: url('{{ asset('images/posters/AOT.png') }}')"></div>
                 </div>
             </div>
         </div>
