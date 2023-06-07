0727bbb61eed88c984fc1148a6449c7893e4487d
Thaofoo <91123146+Thaofoo@users.noreply.github.com>
2023-06-07 10:52:00 +0700

    youtube vid

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -11,9 +11,14 @@
 <h3>{{ $movie->title }} ({{ $movie->year }})</h3>
 
 <div class="player-container">
-    <video id="player" playsinline controls autoplay>
-        <source src="{{ $movie->videolink }}" type="video/mp4">
-    </video>
+    <div class="plyr__video-embed" id="player">
+        <iframe
+          src="https://www.youtube.com/embed/bTqVqk7FSmY?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+          allowfullscreen
+          allowtransparency
+          allow="autoplay"
+        ></iframe>
+      </div>
 
 </div>
 </div>
